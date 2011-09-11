ec6aa55c3df4dd5306be2a75e213957354f84f08
Stanley Hutcheon <stanley.h@hotmail.co.uk>
2011-09-11 20:38:12 +0100

    fixed a few things...

--- UDIDead Bundle Maker/My Project/Resources.Designer.vb
+++ UDIDead Bundle Maker/My Project/Resources.Designer.vb
@@ -59,12 +59,5 @@ Namespace My.Resources
                 resourceCulture = value
             End Set
         End Property
-        
-        Friend ReadOnly Property Ionic_Zip() As Byte()
-            Get
-                Dim obj As Object = ResourceManager.GetObject("Ionic_Zip", resourceCulture)
-                Return CType(obj,Byte())
-            End Get
-        End Property
     End Module
 End Namespace
